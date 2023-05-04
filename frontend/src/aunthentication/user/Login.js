5e2a7f43decbcfc098041d52a88e23b6ac2027f9
Eleazar Odivilas <eleazar.odivilas01@gmail.com>
2023-05-04 18:10:12 +0800

    refactor: remove excess consoles in register and user login

--- frontend/src/aunthentication/user/Login.js
+++ frontend/src/aunthentication/user/Login.js
@@ -2,119 +2,174 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { loginUser } from '../../services/UserService'
 
-function Login( { setIsAuthorized, setIsAdmin } ) {
-    const [userData, setUserData] = useState( {username: "", password: ""} )
+function Login({ setIsAuthorized, setIsAdmin }) {
+    const [userData, setUserData] = useState({ username: '', password: '' })
     const navigate = useNavigate()
     sessionStorage.setItem('navigationOut', false)
 
-  const handleChange = e => {
-    const { name, value } = e.target
-    setUserData( prev => {
-        return { ...prev, [name]: value }
-    })
-    console.log(name, value)
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setUserData((prev) => {
+            return { ...prev, [name]: value }
+        })
     }
 
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         try {
-            loginUser(userData).then((res)=>{
+            loginUser(userData).then((res) => {
                 if (JSON.stringify(res) !== '{}' && res !== undefined) {
                     console.log(res)
 
                     if (res[1].role_id !== 1) {
-                        alert("Not a customer account")
+                        alert('Not a customer account')
                         console.log(res[1].role_id)
-                        navigate('/login')    
+                        navigate('/login')
                     } else {
-                
-                        // Assigning Default Values Upon Login
-                        sessionStorage.setItem("jwt", res[0].jwt)
-                        sessionStorage.setItem("userRole", res[0].userRole)
-                        localStorage.setItem("data", JSON.stringify(res[1]))
+                        // Assigning default values upon login
+                        sessionStorage.setItem('jwt', res[0].jwt)
+                        sessionStorage.setItem('userRole', res[0].userRole)
+                        localStorage.setItem('data', JSON.stringify(res[1]))
 
-                        const data = localStorage.getItem("data")
-                        if (data) {
-                            console.log("Fetch Data: ", JSON.parse(data))
-                        }
-                        alert('Login Successfully! Noice Hahahah')
+                        sessionStorage.getItem('userRole') === '2'
+                            ? setIsAdmin(true)
+                            : setIsAdmin(false)
                         setIsAuthorized(true)
-                        sessionStorage.getItem("userRole") === '2' ? setIsAdmin(true) : setIsAdmin(false) 
-                        navigate('/', { state: res })
-                        // window.location.reload(false)
+
+                        alert('Login Successfully!')
+                        navigate('/')
                     }
-                }
-                else {
-                    console.log("User/Password does not exist!")
-                    alert('User/Password doesn\'t exist!')
+                } else {
+                    alert("User/Password doesn't exist!")
                 }
             })
-            
         } catch (error) {
-            console.log("Error: ", error)
+            console.log('Error: ', error)
         }
     }
 
-  return (
-    <div>
-        <div
-        className="flex justify-left items-center background"
-        style={{
-        backgroundImage: `url(${require('../../assets/img/watery.webp')})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        height: '100vh',
-      }}
-    >
-        <section className="w-[60vw] flex justify-start z-10 ml-6">
-            <div className="w-[35vw] flex flex-cols items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-[35vw] bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Login
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                            <div className="flex">
-                                <div className="flex justify-center items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-l-lg w-12">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 inline text-slate-700 fill-current">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                                    </svg>
-                                </div>
-                                <input type="text" name="username" id="username" placeholder="username" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-r-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required />
-                            </div>
-
-                            <div className="flex">
-                                <div className="flex justify-center items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-l-lg w-12">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="inline text-slate-700 fill-current w-5 h-5">
-                                        <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z" clipRule="evenodd" />
-                                    </svg>
-                                </div>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" onChange={handleChange} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-r-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" required />
-                                </div>
-
-                            <div className="flex items-center justify-start">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5 hidden">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
+    return (
+        <div>
+            <div
+                className="flex justify-left items-center background"
+                style={{
+                    backgroundImage: `url(${require('../../assets/img/watery.webp')})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    height: '100vh',
+                }}
+            >
+                <div className="w-[60vw] flex justify-start z-10 ml-6">
+                    <div className="w-[35vw] flex flex-cols items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                        <div className="w-[35vw] bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Login
+                                </h1>
+                                <form
+                                    className="space-y-4 md:space-y-6"
+                                    onSubmit={handleSubmit}
+                                >
+                                    <div className="flex">
+                                        <div className="flex justify-center items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-l-lg w-12">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="currentColor"
+                                                className="w-5 h-5 inline text-slate-700 fill-current"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            placeholder="username"
+                                            onChange={handleChange}
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-r-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                                            required
+                                        />
                                     </div>
-                                    <div className="ml-3 text-sm hidden">
-                                    <label className="text-gray-500 dark:text-gray-300">Remember me</label>
+
+                                    <div className="flex">
+                                        <div className="flex justify-center items-center bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-l-lg w-12">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                className="inline text-slate-700 fill-current w-5 h-5"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M12 1.5a5.25 5.25 0 00-5.25 5.25v3a3 3 0 00-3 3v6.75a3 3 0 003 3h10.5a3 3 0 003-3v-6.75a3 3 0 00-3-3v-3c0-2.9-2.35-5.25-5.25-5.25zm3.75 8.25v-3a3.75 3.75 0 10-7.5 0v3h7.5z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="••••••••"
+                                            onChange={handleChange}
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-r-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                                            required
+                                        />
                                     </div>
-                                </div>
-                                <a href="/" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 text-orange-500">Forgot password?</a>
+
+                                    <div className="flex items-center justify-between">
+                                        <div className="flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input
+                                                    id="remember"
+                                                    aria-describedby="remember"
+                                                    type="checkbox"
+                                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                                />
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label className="text-gray-500 dark:text-gray-300">
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <button
+                                            onClick={() => navigate('/')}
+                                            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 text-orange-500"
+                                        >
+                                            Forgot password?
+                                        </button>
+                                    </div>
+                                    <button
+                                        type="submit"
+                                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                                    >
+                                        Login
+                                    </button>
+                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Don’t have an account yet?{' '}
+                                        <button
+                                            onClick={() =>
+                                                navigate('/register')
+                                            }
+                                            className="font-medium text-primary-600 hover:underline dark:text-primary-500 text-orange-500"
+                                        >
+                                            Register
+                                        </button>
+                                    </p>
+                                </form>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Login</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <button onClick={() => navigate('/register')} className="font-medium text-primary-600 hover:underline dark:text-primary-500 text-orange-500">Register</button>
-                            </p>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
-  </div>
-  )
+        </div>
+    )
 }
 
 export default Login
--- frontend/src/aunthentication/user/Register.js
+++ frontend/src/aunthentication/user/Register.js
@@ -23,7 +23,7 @@ let defaultAddressValues = {
     city: '',
     country: '',
     tin: '',
-    description: ''
+    description: '',
 }
 
 function Register() {
@@ -42,7 +42,6 @@ function Register() {
         setUserDetails((prev) => {
             return { ...prev, [name]: value }
         })
-        console.log(name, value)
     }
 
     const handleAddressChange = (e) => {
@@ -50,116 +49,120 @@ function Register() {
         setAddressDetails((prev) => {
             return { ...prev, [name]: value }
         })
-        console.log(name, value)
     }
 
     const isPassed = () => {
-        if (user_details.confirm_password === user_details.password
-            && user_details.password !== ""
-            && user_details.username.trim() !== ""
-            && user_details.email.trim() !== ""
-            // && user_details.password.trim() !== ""
-            // && user_details.first_name.trim() !== ""
-            // && user_details.last_name.trim() !== ""
-            // && user_details.age.trim() !== ""
-            // && user_details.gender.trim() !== ""
-            // && user_details.mobile_number.trim() !== ""
-            // && user_details.gender.trim() !== ""
-            // && user_details.address.trim() !== ""
-            // && user_details.baranggay.trim() !== ""
-            // && user_details.city.trim() !== ""
-            // && user_details.country.trim() !== ""
-            ) 
-        {
+        if (
+            user_details.confirm_password === user_details.password &&
+            user_details.password !== '' &&
+            user_details.username.trim() !== '' &&
+            user_details.email.trim() !== ''
+        ) {
             return true
-        }   
-        else {
+        } else {
             return false
         }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
+
         if (isPassed()) {
-            try {
-               registerUser(defaultUserValues, defaultAddressValues)
-               .then((res)=>{
-                        alert('Registered Successfully', res)
-                    })
-                .catch((error) => alert(error))
-                
-            } catch (error) {
-                console.log("Don't Went Through")
-            }
-            navigate('/', { state: user_details })
+            registerUser(defaultUserValues, defaultAddressValues)
+                .then((res) => {
+                    alert('Registered Successfully', res)
+                })
+                .catch((error) => alert("Didn't Went Through"))
+
+            navigate('/')
         } else {
-            setToggle(prev =>!prev)
-            console.log("User Data: ", defaultUserValues)
-            console.log("Address Data: ", defaultAddressValues)
+            setToggle((prev) => !prev)
             alert('Password Mismatched! / Some Fields are empty')
         }
     }
 
-   return (
-    <div>
-        <div
-        className="flex justify-left items-center background"
-        style={{
-            backgroundImage: `url(${require('../../assets/img/watery.webp')})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            height: '100vh',
-        }} >
-            <section className="!w-[60vw] h-[100vh] flex justify-start z-10 ml-8">
-                <div className="flex flex-col items-center justify-start px-6 mx-auto md:h-screen lg:py-0">
-                    <div className="!w-full g-white !mt-8 rounded-lg shadow dark:border h-[90vh] md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700">
-                        <div className="p-6 h-[90vh] space-y-6 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Register
-                            </h1>
-                            <form className="" onSubmit={handleSubmit}>
+    return (
+        <div>
+            <div
+                className="flex justify-left items-center background"
+                style={{
+                    backgroundImage: `url(${require('../../assets/img/watery.webp')})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    height: '100vh',
+                }}
+            >
+                <div className="!w-[60vw] h-[100vh] flex justify-start z-10 ml-8">
+                    <div className="flex flex-col items-center justify-start px-6 mx-auto md:h-screen lg:py-0">
+                        <div className="!w-full g-white !mt-8 rounded-lg shadow dark:border h-[90vh] md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 h-[90vh] space-y-6 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Register
+                                </h1>
+                                <form className="" onSubmit={handleSubmit}>
                                     <div>
-                                {isToggle ? 
-                                    <div>
-                                        <DeliveryAddressForm defaultAddressValues={defaultAddressValues} handleAddressChange={handleAddressChange} />
-                                    </div>
-                                   : 
-                                   <PersonalDataForm defaultUserValues={defaultUserValues} handleUserChange={handleUserChange}/>
-                                    }
-                                
-                                {isToggle ? ( 
-                                     <>
-                                        <div className="pt-6 flex items-end text-white text-xs font-small">Address will be your delivery address for your orders.</div>
-                                        <div className="flex items-end h-16 grow mt-2">
-                                            <button type="submit" className="grow rounded-sm text-white bg-primary-600 hover:bg-primary-700 font-medium text-sm px-2 text-center dark:bg-green-600 dark:hover:bg-green-700 h-10"
-                                    >
-                                        Register Now
-                                            </button> 
-                                        </div>
-                                     </>
-                                ) : (<></>)}
+                                        {isToggle ? (
+                                            <div>
+                                                <DeliveryAddressForm
+                                                    defaultAddressValues={
+                                                        defaultAddressValues
+                                                    }
+                                                    handleAddressChange={
+                                                        handleAddressChange
+                                                    }
+                                                />
+                                            </div>
+                                        ) : (
+                                            <PersonalDataForm
+                                                defaultUserValues={
+                                                    defaultUserValues
+                                                }
+                                                handleUserChange={
+                                                    handleUserChange
+                                                }
+                                            />
+                                        )}
 
-                                    <div className="flex items-center h-16 grow mt-2">
-                                        <button
-                                            // type="submit"
-                                            type="button"
-                                            onClick={() => setToggle(prev =>!prev)}
-                                            className="grow rounded-sm text-white bg-primary-600 hover:bg-primary-700 font-medium text-sm px-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 h-10"
-                                        >
-                                            {
-                                                isToggle ? "Previous" : "Next"
-                                            }
-                                        </button>
+                                        {isToggle ? (
+                                            <>
+                                                <div className="pt-6 flex items-end text-white text-xs font-small">
+                                                    Address will be your
+                                                    delivery address for your
+                                                    orders.
+                                                </div>
+                                                <div className="flex items-end h-16 grow mt-2">
+                                                    <button
+                                                        type="submit"
+                                                        className="grow rounded-sm text-white bg-primary-600 hover:bg-primary-700 font-medium text-sm px-2 text-center dark:bg-green-600 dark:hover:bg-green-700 h-10"
+                                                    >
+                                                        Register Now
+                                                    </button>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <></>
+                                        )}
+
+                                        <div className="flex items-center h-16 grow mt-2">
+                                            <button
+                                                type="button"
+                                                onClick={() =>
+                                                    setToggle((prev) => !prev)
+                                                }
+                                                className="grow rounded-sm text-white bg-primary-600 hover:bg-primary-700 font-medium text-sm px-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 h-10"
+                                            >
+                                                {isToggle ? 'Previous' : 'Next'}
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
         </div>
-    </div>
-   )
+    )
 }
 
 export default Register
