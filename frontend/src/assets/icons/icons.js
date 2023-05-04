774b8661f7bf2209e564056faa482e88040d3d15
Eleazar Odivilas <eleazar.odivilas01@gmail.com>
2023-05-04 19:37:23 +0800

    refactor: refactor side bar again

--- frontend/src/assets/icons/icons.js
+++ frontend/src/assets/icons/icons.js
@@ -1,20 +1,33 @@
 import {
     AiOutlineCaretDown,
     AiOutlineCaretUp,
+    AiOutlineHistory,
     AiOutlineMinus,
     AiOutlinePlus,
+    AiOutlineUser,
 } from 'react-icons/ai'
+import { BiPurchaseTagAlt } from 'react-icons/bi'
 import { FaCartArrowDown, FaHome } from 'react-icons/fa'
+import { FiShoppingCart } from 'react-icons/fi'
 import { GiWaterBottle } from 'react-icons/gi'
-import { MdLogout } from 'react-icons/md'
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { MdLogout, MdOutlineDashboard } from 'react-icons/md'
+import { RiSettings4Line } from 'react-icons/ri'
 
 export {
     AiOutlineCaretDown,
     AiOutlineCaretUp,
+    AiOutlineHistory,
     AiOutlinePlus,
     AiOutlineMinus,
+    AiOutlineUser,
+    BiPurchaseTagAlt,
     FaCartArrowDown,
     FaHome,
+    FiShoppingCart,
     GiWaterBottle,
+    HiMenuAlt3,
     MdLogout,
+    MdOutlineDashboard,
+    RiSettings4Line,
 }
--- frontend/src/common/Sidebar.js
+++ frontend/src/common/Sidebar.js
@@ -1,122 +1,139 @@
-import React, { useState } from "react";
-import { HiMenuAlt3 } from "react-icons/hi";
-import { MdOutlineDashboard, MdLogout } from "react-icons/md";
-import { RiSettings4Line } from "react-icons/ri";
-import { AiOutlineUser, AiOutlineHistory } from "react-icons/ai";
-import { BiPurchaseTagAlt } from "react-icons/bi";
-import { FiShoppingCart } from "react-icons/fi";
-import { Link, useNavigate } from "react-router-dom";
-import { isAdmin_ } from "../App";
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import {
+    AiOutlineHistory,
+    AiOutlineUser,
+    BiPurchaseTagAlt,
+    FiShoppingCart,
+    HiMenuAlt3,
+    MdOutlineDashboard,
+    MdLogout,
+    RiSettings4Line,
+} from '../assets/icons/icons'
+
+import { isAdmin_ } from '../App'
 
 const adminMenus = [
-  { name: "Dashboard", link: "/", icon: MdOutlineDashboard },
-  { name: "Users", link: "/admin/users", icon: AiOutlineUser },
-  { name: "Orders", link: "/admin/orders", icon: FiShoppingCart },
-  { name: "Setting", link: "/", icon: RiSettings4Line },
-  { name: "Logout", link: "/", icon: MdLogout },
-
-];
-
-const userMenus = [
-  {name: "Dashboard", link: "/", icon: MdOutlineDashboard}, 
-  {name: "Order Now", link: "/order", icon: BiPurchaseTagAlt}, 
-  {name: "Orders", link: "/users/orders", icon: FiShoppingCart},
-  {name: "History", link: "/", icon: AiOutlineHistory}, 
-  {name: "Setting", link: "/", icon: RiSettings4Line},
-  { name: "Logout", link: "/", icon: MdLogout },
+    { name: 'Dashboard', link: '/', icon: MdOutlineDashboard },
+    { name: 'Users', link: '/admin/users', icon: AiOutlineUser },
+    { name: 'Orders', link: '/admin/orders', icon: FiShoppingCart },
+    { name: 'Setting', link: '/', icon: RiSettings4Line },
+    { name: 'Logout', link: '/', icon: MdLogout },
 ]
 
-const Sidebar = ({open, setOpen, setIsAuthorized, setIsAdmin }) => {
-  const [menus, setMenus] = useState( isAdmin_ ? adminMenus : userMenus)
+const userMenus = [
+    { name: 'Dashboard', link: '/', icon: MdOutlineDashboard },
+    { name: 'Order Now', link: '/order', icon: BiPurchaseTagAlt },
+    { name: 'Orders', link: '/users/orders', icon: FiShoppingCart },
+    { name: 'History', link: '/', icon: AiOutlineHistory },
+    { name: 'Setting', link: '/', icon: RiSettings4Line },
+    { name: 'Logout', link: '/', icon: MdLogout },
+]
 
-  const navigate = useNavigate()
-  const handleLogout = () => {
-    sessionStorage.removeItem("jwt")
-    localStorage.removeItem("data")
-    // This is added for the sided to append
-    setIsAuthorized(false)
-    setIsAdmin(false)
-    navigate('/admin')
-  }
+const Sidebar = ({ open, setOpen, setIsAuthorized, setIsAdmin }) => {
+    const menus = isAdmin_ ? adminMenus : userMenus
 
-  return (
-    // <section className="flex gap-6">
-      <section>
-      <div
-        className={`bg-[#0e0e0e] min-h-screen ${
-          open ? "w-60" : "w-16"
-        } duration-500 text-gray-100 px-4`}
-      >
-        <div className="py-3 flex justify-end">
-          <HiMenuAlt3
-            size={26}
-            className="cursor-pointer"
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-        <div className="mt-4 flex flex-col gap-4 relative">
-          {/* { sessionStorage.getItem('jwt') ? setMenus(adminMenus) : setMenus(userMenus) } */}
-          {menus?.map((menu, i) => (
-            <>
-              {menu.name === "Logout" ?
-              <>
-              <button onClick={handleLogout} className={` ${
-                    menu?.margin && "mt-5"
-                  } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}>
-                <div>{React.createElement(menu?.icon, { size: "20" })}</div>
-                  <h2
-                    style={{
-                      transitionDelay: `${i + 3}00ms`,
-                    }}
-                    className={`whitespace-pre duration-500 ${
-                      !open && "opacity-0 translate-x-28 overflow-hidden"
-                    }`}
-                  >
-                    {menu?.name}
-                  </h2>
-                  <h2
-                    className={`${
-                      open && "hidden"
-                    } absolute left-40 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit z-50`}
-                  >
-                    {menu?.name}
-                  </h2>
-              </button>
-              </> : 
-                 ( <Link
-                  to={menu?.link}
-                  key={i}
-                  className={` ${
-                    menu?.margin && "mt-5"
-                  } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
-                >
-                  <div>{React.createElement(menu?.icon, { size: "20" })}</div>
-                  <h2
-                    style={{
-                      transitionDelay: `${i + 3}00ms`,
-                    }}
-                    className={`whitespace-pre duration-500 ${
-                      !open && "opacity-0 translate-x-28 overflow-hidden"
-                    }`}
-                  >
-                    {menu?.name}
-                  </h2>
-                  <h2
-                    className={`${
-                      open && "hidden"
-                    } absolute left-40 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit z-50`}
-                  >
-                    {menu?.name}
-                  </h2>
-                </Link>
-              )}
-            </>
-          ))}
-          
-        </div>
-      </div>
-    </section>
-  );
-};
+    const navigate = useNavigate()
 
-export default Sidebar;
+    const handleLogout = () => {
+        sessionStorage.removeItem('jwt')
+        localStorage.removeItem('data')
+        setIsAuthorized(false)
+        setIsAdmin(false)
+        navigate('/admin')
+    }
+
+    return (
+        <section>
+            <div
+                className={`bg-[#0e0e0e] min-h-screen ${
+                    open ? 'w-60' : 'w-16'
+                } duration-500 text-gray-100 px-4`}
+            >
+                <div className="py-3 flex justify-end">
+                    <HiMenuAlt3
+                        size={26}
+                        className="cursor-pointer"
+                        onClick={() => setOpen(!open)}
+                    />
+                </div>
+                <div className="mt-4 flex flex-col gap-4 relative">
+                    {/* { sessionStorage.getItem('jwt') ? setMenus(adminMenus) : setMenus(userMenus) } */}
+                    {menus?.map((menu, i) => (
+                        <>
+                            {menu.name === 'Logout' ? (
+                                <>
+                                    <button
+                                        onClick={handleLogout}
+                                        className={` ${
+                                            menu?.margin && 'mt-5'
+                                        } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+                                    >
+                                        <div>
+                                            {React.createElement(menu?.icon, {
+                                                size: '20',
+                                            })}
+                                        </div>
+                                        <h2
+                                            style={{
+                                                transitionDelay: `${i + 3}00ms`,
+                                            }}
+                                            className={`whitespace-pre duration-500 ${
+                                                !open &&
+                                                'opacity-0 translate-x-28 overflow-hidden'
+                                            }`}
+                                        >
+                                            {menu?.name}
+                                        </h2>
+                                        <h2
+                                            className={`${
+                                                open && 'hidden'
+                                            } absolute left-40 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit z-50`}
+                                        >
+                                            {menu?.name}
+                                        </h2>
+                                    </button>
+                                </>
+                            ) : (
+                                <Link
+                                    to={menu?.link}
+                                    key={i}
+                                    className={` ${
+                                        menu?.margin && 'mt-5'
+                                    } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+                                >
+                                    <div>
+                                        {React.createElement(menu?.icon, {
+                                            size: '20',
+                                        })}
+                                    </div>
+                                    <h2
+                                        style={{
+                                            transitionDelay: `${i + 3}00ms`,
+                                        }}
+                                        className={`whitespace-pre duration-500 ${
+                                            !open &&
+                                            'opacity-0 translate-x-28 overflow-hidden'
+                                        }`}
+                                    >
+                                        {menu?.name}
+                                    </h2>
+                                    <h2
+                                        className={`${
+                                            open && 'hidden'
+                                        } absolute left-40 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit z-50`}
+                                    >
+                                        {menu?.name}
+                                    </h2>
+                                </Link>
+                            )}
+                        </>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar
